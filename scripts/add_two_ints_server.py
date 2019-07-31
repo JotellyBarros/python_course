0c34ebfad7b182cc358c3a8812fb416aca992972
Jotelly Barros <jotelly@gmail.com>
2019-07-31 09:10:16 -0300

    Class Power Supple Debug

--- scripts/add_two_ints_server.py
+++ scripts/add_two_ints_server.py
@@ -5,8 +5,7 @@ import rospy
 from std_srvs.srv import SetBool
 from python_course.srv import *
 from python_course.msg import *
-# from std_msgs.msg import Float64
-from power_supply import *  # Class Power Supple
+from debug_power_supply import *  # Class Power Supple Debug
 
 
 # Changes the power supply of the source
@@ -14,16 +13,20 @@ def callback_output(req):
     return supply.set_enable_output(req.data)
 
 # Changes the mode supply voltage
+
+
 def callback_voltage(req):
     return supply.set_voltage_set_point(req.data)
 
 # Changes the mode supply current
+
+
 def callback_current(req):
     return supply.set_current_set_point(req.data)
 
 
 def server_supply():
-    rospy.init_node('server_supply', anonymous=True)
+    rospy.init_node('server_supply', anonymous=False)
 
     rospy.Service('enable_output', SetBool, callback_output)
     rospy.Service('set_voltage', SetFloat64, callback_voltage)
@@ -36,8 +39,6 @@ def server_supply():
     while not rospy.is_shutdown():
         new_msg = PowerSupplyState()
 
-        # new_msg.op_mode = new_msg.VOLTAGE_MODE
-
         new_msg.enable_output = supply.enable_output
         new_msg.op_mode = supply.op_mode
         new_msg.voltage_set_point = supply.voltage_set_point
@@ -46,13 +47,11 @@ def server_supply():
         new_msg.current = supply.current
 
         rospy.loginfo("\nMessages from Server:\n{}".format(new_msg))
-        # pub.publish(new_msg)
-        rate.sleep()
+        pub.publish(new_msg)
 
+        rate.sleep()
     rospy.spin()
 
-
 if __name__ == "__main__":
-    supply = SupplyParameters()
-    # supply.printStatus()
+    supply = DebugSupplyParameters()
     server_supply()
